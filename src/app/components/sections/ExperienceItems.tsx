ecf377a9b4ad43daf1bf8e11b73a2712aaec9748
Michael Bullet <bulletcarry@gmail.com>
2025-08-29 18:41:59 -0300

    Update position title for Rede Materdei de Saúde

--- src/app/components/sections/ExperienceItems.tsx
+++ src/app/components/sections/ExperienceItems.tsx
@@ -90,7 +90,7 @@ const experienceLists: experienceListsProps[] = [
   {
     imageSrc: "/asu.svg",
     companyName: "Rede Materdei de Saúde - Hospital EMEC",
-    positionTitle: "Sysadmin & DevOps",
+    positionTitle: "Sysadmin",
     date: "Fevereiro de 2022 – Julho de 2023",
   },
   {
