5142d0d1b27f36f0f384c7a65be1b30311a501bf
Michael D. <contato@michaelbullet.com>
2025-10-09 15:24:37 -0300

    feat: adjust resume link

--- src/app/components/NavButton.tsx
+++ src/app/components/NavButton.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 export default function NavButton({ name }: ButtonProps) {
   return (
     <a
-      href="https://drive.google.com/file/d/1cyBvxwwdxf_K24ei8V41gGARHuaQq32C/view?usp=sharing" 
+      href="https://drive.google.com/file/d/1rFuuVt4OD5Yh8NHbF7rdv4DT9MMdmz6O/view?usp=sharing" 
       target="_blank"
     >
       <button
